051ff7627c3e033b881f878778a8cd7bbf5410dc
James Adelhelm <adelhelj@oregonstate.edu>
2024-01-26 13:06:43 -0800

    Configured GitHub social media authentication using Firebase

--- frontend/src/firebaseConfig.js
+++ frontend/src/firebaseConfig.js
@@ -1,16 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  // Firebase config api key info paste in here
-  
+  apiKey: "your-api-key",
+  authDomain: "opm-webapp.firebaseapp.com",
+  projectId: "opm-webapp",
+  storageBucket: "opm-webapp.appspot.com",
+  messagingSenderId: "your-messaging-sender-id",
+  appId: "your-app-id"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);

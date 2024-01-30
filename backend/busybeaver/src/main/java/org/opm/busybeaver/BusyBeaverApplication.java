5e8a3ee59fee72f3004983ef39546c2e48cee9a6
Giovanni Propersi <80492028+GPropersi@users.noreply.github.com>
2024-01-30 15:11:26 -0600

    Initial configuration of Spring server (#20)

---
+++ backend/busybeaver/src/main/java/org/opm/busybeaver/BusyBeaverApplication.java
@@ -0,0 +1,13 @@
+package org.opm.busybeaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusyBeaverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusyBeaverApplication.class, args);
+    }
+
+}
---
+++ backend/busybeaver/src/test/java/org/opm/busybeaver/BusybeaverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.opm.busybeaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusybeaverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

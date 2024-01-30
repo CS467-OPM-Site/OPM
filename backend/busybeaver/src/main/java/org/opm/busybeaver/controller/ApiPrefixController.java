0909a190074f8007fb0a65170cbec96315307084
Giovanni Propersi <giovannigp@gmail.com>
2024-01-30 16:41:36 -0600

    Allow endpoints to be versioned via a controller

---
+++ backend/busybeaver/src/main/java/org/opm/busybeaver/controller/ApiPrefixController.java
@@ -0,0 +1,17 @@
+package org.opm.busybeaver.controller;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Component
+@RequestMapping("/v1")
+public @interface  ApiPrefixController {
+    @AliasFor(annotation = Component.class)
+    String value() default "";
+}

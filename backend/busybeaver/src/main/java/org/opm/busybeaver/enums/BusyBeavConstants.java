20f6018610ab3d8a12c1ad1dd2bb98cb7062b245
Giovanni Propersi <giovannigp@gmail.com>
2024-01-31 16:24:09 -0600

    Remove HelloWorld, add constant enums, update packages

---
+++ backend/busybeaver/src/main/java/org/opm/busybeaver/enums/BusyBeavConstants.java
@@ -0,0 +1,19 @@
+package org.opm.busybeaver.enums;
+
+public enum BusyBeavConstants {
+    USER_KEY_VAL("User"),
+    ERROR_KEY_VAL("Error"),
+    JSON_CONTENT_TYPE("application/json"),
+    UTF_8_ENCODING("UTF-8"),
+    RESOURCES_DIR("./src/main/resources/");
+
+    private final String value;
+
+    BusyBeavConstants(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}

e37c7f9ed39a540bad730d48106bd66e65a170aa
Giovanni Propersi <80492028+GPropersi@users.noreply.github.com>
2024-01-31 21:44:59 -0600

    Allow endpoints to be versioned via a controller (#23)
    Base method to allow for versioning of the API.

--- backend/busybeaver/src/main/java/org/opm/busybeaver/HelloWorldController.java
+++ backend/busybeaver/src/main/java/org/opm/busybeaver/HelloWorldController.java
@@ -1,9 +1,11 @@
 package org.opm.busybeaver;
 
+import org.opm.busybeaver.controller.ApiPrefixController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@ApiPrefixController
 public class HelloWorldController {
 
     private static final String template = "Welcome, to Busy Beaver Project Management!";

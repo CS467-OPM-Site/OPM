a8a43ac3067a3b57682f95fa2d2bc60c60a4016f
James Adelhelm <adelhelj@oregonstate.edu>
2024-02-01 19:08:12 -0800

    feat: Navigate authenticated users to /home and initiate homepage design
    This commit introduces a new feature where users authenticated via Firebase are now redirected to the /home route, aligning with our user flow requirements. Additionally, it lays the groundwork for the homepage design, which is currently in a work-in-progress state. The design follows our initial Figma prototype and will be refined in subsequent updates. Authenticated users can expect a seamless transition to a the user home page

--- frontend/src/components/SplashPage.js
+++ frontend/src/components/SplashPage.js
@@ -1,32 +1,21 @@
 import React from 'react';
-
+import { useNavigate } from 'react-router-dom';
 import '../styles/SplashPage.css';
-import { getAuth, signInWithPopup, GoogleAuthProvider, GithubAuthProvider, createUserWithEmailAndPassword } from 'firebase/auth';
-import { initializeApp } from 'firebase/app';
 import app from '../firebaseConfig';
+import { getAuth, signInWithPopup, GoogleAuthProvider, GithubAuthProvider } from 'firebase/auth';
 
 const SplashPage = () => {
+  const navigate = useNavigate();
 
   const handleGoogleSignIn = () => {
     const auth = getAuth();
     const provider = new GoogleAuthProvider();
     signInWithPopup(auth, provider)
       .then((result) => {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        // const credential = GoogleAuthProvider.credentialFromResult(result);
-        // const token = credential.accessToken;
-        // The signed-in user info.
-        // const user = result.user;
-        // ...
+        // After sign-in, navigate to the UserHomepage
+        navigate('/home');
       }).catch((error) => {
         // Handle Errors here.
-        // const errorCode = error.code;
-        // const errorMessage = error.message;
-        // The email of the user's account used.
-        // const email = error.email;
-        // The AuthCredential type that was used.
-        // const credential = GoogleAuthProvider.credentialFromError(error);
-        // ...
       });
   }
 
@@ -35,43 +24,28 @@ const SplashPage = () => {
     const provider = new GithubAuthProvider();
     signInWithPopup(auth, provider)
       .then((result) => {
-        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-        // const credential = GithubAuthProvider.credentialFromResult(result);
-        // const token = credential.accessToken;
-        // The signed-in user info.
-        // const user = result.user;
-        // ...
-      }).catch((error) => {
+        // After sign-in, navigate to the UserHomepage
+        navigate('/home');
+      })
+      .catch((error) => {
         // Handle Errors here.
-        // const errorCode = error.code;
-        // const errorMessage = error.message;
-        // The email of the user's account used.
-        // const email = error.email;
-        // The AuthCredential type that was used.
-        // const credential = GithubAuthProvider.credentialFromError(error);
-        // ...
       });
-  }
+  };
 
-  
   return (
     <div className="splash-container">
       <h1>Let us solve your project management needs.</h1>
-      <h4>Our Opinionated Project Management software manages  projects efficiently and effectively.</h4>
+      <h4>Our Opinionated Project Management software manages projects efficiently and effectively.</h4>
       <p>Team Members: James Adelhelm, Ryu Barrett, Giovanni Propersi</p>
       <p>Course: CS467 Online Capstone Project W2024.</p>
       <div className="auth-buttons">
-
         <button className="google-sign-in" onClick={handleGoogleSignIn}>
-          {/* If you have a Google icon, insert it here */}
           <span>Sign In with Google</span>
         </button>
-        <button className="github-sign-in" onClick={handleGithubSignIn} >
-          {/* If you have a Google icon, insert it here */}
+        <button className="github-sign-in" onClick={handleGithubSignIn}>
           <span>Sign In with GitHub</span>
         </button>
       </div>
-      {/* Add any other content you want on your splash page */}
     </div>
   );
 };

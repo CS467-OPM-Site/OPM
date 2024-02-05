a7234639c4fbbac9200411f06313e9cbeb76b767
Giovanni Propersi <giovannigp@gmail.com>
2024-02-04 22:24:57 -0600

    jooq configuration setup, classes can be generated from sql database

---
+++ backend/busybeaver/src/main/java/org/opm/busybeaver/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package org.opm.busybeaver.repository;
+
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+
+}
--- backend/busybeaver/src/test/java/org/opm/busybeaver/BusybeaverApplicationTests.java
+++ backend/busybeaver/src/test/java/org/opm/busybeaver/BusybeaverApplicationTests.java
@@ -1,10 +1,16 @@
 package org.opm.busybeaver;
 
 import org.junit.jupiter.api.Test;
+import org.opm.busybeaver.config.FirebaseConfig;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class BusybeaverApplicationTests {
+
+    // Need to mock this due to System properties call in FirebaseConfig.java not loading
+    @MockBean
+    private FirebaseConfig firebaseConfig;
 
     @Test
     void contextLoads() {

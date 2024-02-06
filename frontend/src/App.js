666cdc4b3d9bf09b713ef36f04b14877f2c89f9f
James Adelhelm <102548637+adelhelj@users.noreply.github.com>
2024-02-05 18:17:04 -0800

    feat: Navigate authenticated users to /home and initiate homepage design (#30)
    This commit introduces a new feature where users authenticated via Firebase are now redirected to the /home route, aligning with our user flow requirements. Additionally, it lays the groundwork for the homepage design, which is currently in a work-in-progress state. The design follows our initial Figma prototype and will be refined in subsequent updates. Authenticated users can expect a seamless transition to a the user home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './styles/App.css';
 import SplashPage from './components/SplashPage';
+import UserHomePage from './components/UserHomepage';
 
 function App() {
   return (
-    <div className="App">
-      <SplashPage />
-      {/* You can add more components here as your app grows */}
-    </div>
+    <Router>
+      <div className="App">
+        {/* Define your routes within the Routes component */}
+        <Routes>
+          {/* The path "/" corresponds to the SplashPage */}
+          <Route path="/" element={<SplashPage />} />
+          {/* The path "/home" corresponds to the UserHomePage */}
+          <Route path="/home" element={<UserHomePage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 

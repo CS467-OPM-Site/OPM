eb9c8ad01a870b94315a06ef45738900963daf5b
Giovanni Propersi <giovannigp@gmail.com>
2024-02-08 14:13:50 -0600

    Added endpoint for adding member to team

---
+++ backend/busybeaver/src/main/java/org/opm/busybeaver/exceptions/Users/UserAlreadyInTeamException.java
@@ -0,0 +1,11 @@
+package org.opm.busybeaver.exceptions.service;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class UserAlreadyInTeamException extends RuntimeException {
+    public UserAlreadyInTeamException(String errorMessage) {
+        super(errorMessage);
+    }
+}

527a9021460e3d4e8015eab4b37f8ca95fd1ecf9
James Adelhelm <102548637+adelhelj@users.noreply.github.com>
2024-02-15 10:21:27 -0800

    Implement user auth flow in SplashPage component (#56)
    - Add signInWithPopup for Google and GitHub providers.
    - Handle Firebase auth/account-exists-with-different-credential error.
    - Fetch to /users/auth to authenticate user with backend upon successful sign-in.
    - Redirect to /username for registration if user does not exist (404 response).
    - Navigate to /home if user authentication is successful.
    
    This change improves the user sign-in experience by properly managing different authentication scenarios and streamlining the registration process for new users.

--- frontend/src/components/SplashPage.js
+++ frontend/src/components/SplashPage.js
@@ -3,7 +3,6 @@ import '../styles/SplashPage.css';
 import app from '../firebaseConfig';
 // import the image file from the assets folder
 import { getAuth, signInWithPopup, GoogleAuthProvider, GithubAuthProvider } from 'firebase/auth';
-import BusyBeaverImage from '../assets/BusyBeaverImage.webp';
 import BusyBeaverNoBG from '../assets/BusyBeaverNoBG.png';
 // // Citation for BusyBeaverImage: OpenAI DALL E Image Generator (https://openai.com/research/dall-e/)
 // Downloaded image at 2024-02-07 14:27:00 with prompt "generate some good logos to go on the front page and maybe like a corner icon for this project that match the css. It's our senior computer science capstone project at Oregon State university and we want our project management site to be called BusyBeaver (like oregon state beavers)"
@@ -11,49 +10,67 @@ import BusyBeaverNoBG from '../assets/BusyBeaverNoBG.png';
 const SplashPage = () => {
   const navigate = useNavigate();
 
-  const handleGoogleSignIn = () => {
-    const auth = getAuth();
-    const provider = new GoogleAuthProvider();
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        // After sign-in, navigate to the UserHomepage
-        navigate('/username');
-      }).catch((error) => {
-        // Handle Errors here.
-      });
-  }
+  const authenticateUserWithBackend = async (idToken) => {
+    // Attempt to authenticate the user with the backend
+    const response = await fetch('https://opm-api.propersi.me/api/v1/users/auth', {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${idToken}`,
+        'Content-Type': 'application/json',
+      },
+    });
 
-  const handleGithubSignIn = () => {
-    const auth = getAuth();
-    const provider = new GithubAuthProvider();
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        // After sign-in, navigate to the UserHomepage
-        navigate('/username');
-      })
-      .catch((error) => {
-        if (error.code === 'auth/account-exists-with-different-credential') {
-          // Display message to user if they already have an account with the same email address but different sign-in method
-          alert("An account with this email already exists. Please sign in using your existing Google account.");
-        } 
-      });
+    // Check the response from the backend
+    if (response.ok) {
+      // If authentication is successful (200), navigate to the user's home page
+      navigate('/home');
+    } else if (response.status === 404) {
+      // If user does not exist (404), redirect to the registration page
+      navigate('/username', { state: { idToken } });
+    } else {
+      // Handle other responses or errors
+      console.error('An error occurred during authentication');
+    }
   };
 
-  
+  const handleSignIn = async (provider) => {
+    const auth = getAuth();
+    signInWithPopup(auth, provider).then(async (result) => {
+      const idToken = await auth.currentUser.getIdToken();
+      // Use the ID token to authenticate with the backend
+      authenticateUserWithBackend(idToken);
+    }).catch((error) => {
+      // Handle Errors here.
+      if (error.code === 'auth/account-exists-with-different-credential') {
+        // Handle account exists with different credential error
+        alert("An account with this email already exists. Please sign in using your existing account.");
+      } else {
+        console.error('Authentication error with Firebase:', error);
+      }
+    });
+  };
+
+  const handleGoogleSignIn = () => {
+    const provider = new GoogleAuthProvider();
+    handleSignIn(provider);
+  };
+
+  const handleGithubSignIn = () => {
+    const provider = new GithubAuthProvider();
+    handleSignIn(provider);
+  };
+
   return (
     <div className="splash-container">
-      
       <h1>Welcome to BusyBeaver</h1>
-      <img src={BusyBeaverNoBG} alt="Busy Beaver" />  
+      <img src={BusyBeaverNoBG} alt="Busy Beaver" />
       <h4>Let us solve your project management needs.</h4>
-      <p>Team Members: James Adelhelm, Ryu Barrett, Giovanni Propersi</p>
-      <p>Course: CS467 Online Capstone Project W2024.</p>
       <div className="auth-buttons">
         <button className="google-sign-in" onClick={handleGoogleSignIn}>
-          <span>Sign In with Google</span>
+          Sign In with Google
         </button>
         <button className="github-sign-in" onClick={handleGithubSignIn}>
-          <span>Sign In with GitHub</span>
+          Sign In with GitHub
         </button>
       </div>
     </div>

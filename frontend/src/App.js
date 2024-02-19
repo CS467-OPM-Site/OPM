7b953f36a1fb3a080a43e1161920b2575d170518
Giovanni Propersi <80492028+GPropersi@users.noreply.github.com>
2024-02-19 01:20:25 -0600

    Deployment on cloudflare pages (#70)
    * Attempt to fix cloudflare failure by changing app initialization
    
    * Cleanup for hosting on cloudflare pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,6 @@
+import React, { useEffect, useState } from 'react';
+import { initializeApp } from 'firebase/app';
+import { firebaseConfig } from './firebaseConfig';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext'; // Make sure the path matches where your AuthContext.js is located
 import './styles/App.css';
@@ -5,7 +8,24 @@ import SplashPage from './components/SplashPage';
 import UserHomePage from './components/UserHomepage';
 import UsernameSetup from './components/UsernameSetup';
 
+
 function App() {
+
+
+  const [isFirebaseInitialized, setIsFirebaseInitialized] = useState(false);
+
+  useEffect(() => {
+    const app = initializeApp(firebaseConfig);
+
+    setIsFirebaseInitialized(true);
+  }, []);
+
+  if (!isFirebaseInitialized) {
+    // Wait until firebase is initialized before doing anything else, or
+    // the AuthContext will fail
+    return <div>Loading...</div>;
+  }
+
   return (
     <Router>
       <div className="App">
--- frontend/src/firebaseConfig.js
+++ frontend/src/firebaseConfig.js
@@ -1,10 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
@@ -13,5 +9,3 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);

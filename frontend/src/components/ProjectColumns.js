9149f1c343b72ce959f7c1c4efdbc73c70db2bcd
Giovanni Propersi <80492028+GPropersi@users.noreply.github.com>
2024-02-25 13:42:49 -0600

    Initializing project management page (#89)
    * Initializing project management page
    
    * Fix package-lock for cloudflare deployment
    
    * Add dependency on old package to allow build....

---
+++ frontend/src/components/ProjectColumns.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+const ProjectColumn = ( {columnTitle, columnID} ) => {
+
+  return <>
+        <div className="column-card" key={columnID}>
+            <Typography variant="h5" component="div" className={columnTitle}>
+              {columnTitle}
+            </Typography>
+          </div>
+      </>
+}
+
+export default ProjectColumn;

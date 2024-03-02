b97bb6d8f8505a3370e468caa8afd931738e9d47
Giovanni Propersi <giovannigp@gmail.com>
2024-03-01 20:48:21 -0600

    Add border animation

--- frontend/src/components/ProjectTasks.js
+++ frontend/src/components/ProjectTasks.js
@@ -3,9 +3,13 @@ import { Typography } from "@mui/material";
 import { DriveFileRenameOutlineTwoTone, DeleteTwoTone, KeyboardDoubleArrowLeft, KeyboardDoubleArrowRight } from "@mui/icons-material";
 
 
+const BASE_TASK_CONTAINER_CLASS = "task-summary-container"
+const HOVER_TASK_CONTAINER = "task-summary-container-hover-border"
+const MOUSE_EXIT_TASK_CONTAINER = "task-summary-container-unhover-border"
 const BASE_ICONS_CONTAINER_CLASS = "task-summary-icon-menu-bar-container"
 const SHOW_ICONS = "show-task-summary-icon-container"
 const HIDE_ICONS = "hide-task-summary-icon-container"
+
 
 const ProjectTask = ({ currentTask }) => {
   const [shouldPlayHideSlideAnimation, setShouldPlayHideSlideAnimation] = useState(0);
@@ -29,8 +33,22 @@ const ProjectTask = ({ currentTask }) => {
     }
   }
 
+  const setTaskContainerClass = () => {
+    switch (shouldPlayHideSlideAnimation) {
+      case 0: return BASE_TASK_CONTAINER_CLASS;
+      case 1: {
+        return `${BASE_TASK_CONTAINER_CLASS} ${HOVER_TASK_CONTAINER}`
+      } 
+      case 2: {
+        return `${BASE_TASK_CONTAINER_CLASS} ${MOUSE_EXIT_TASK_CONTAINER}`
+      }
+    }
+  }
+
+  
+
   return <>
-    <div className="task-summary-container" onMouseLeave={handleOnMouseOut} onMouseEnter={() => {setShouldPlayHideSlideAnimation(1)}}>
+    <div className={setTaskContainerClass()} onMouseLeave={handleOnMouseOut} onMouseEnter={() => {setShouldPlayHideSlideAnimation(1)}}>
       <Typography className="task-summary-title" sx={{ fontWeight: 700, fontSize: '0.9rem' }} gutterBottom={true}>{currentTask.title}</Typography>
       <Typography sx={{ fontWeight: 500, fontSize: '0.9rem' }}>Priority: {currentTask.priority}</Typography>
       <div className="task-summary-due-date-comments-container">
